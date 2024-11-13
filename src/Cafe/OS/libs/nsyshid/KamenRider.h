973b0dc41486c8bfdc95efcd3b05edaa7d26c31d
Joshua de Reeper <joshua@dereeper.co.nz>
2024-11-13 17:08:33 +0100

    first methods for commands

--- src/Cafe/OS/libs/nsyshid/KamenRider.h
+++ src/Cafe/OS/libs/nsyshid/KamenRider.h
@@ -5,6 +5,8 @@
 #include "nsyshid.h"
 #include "Backend.h"
 
+#include "Common/FileStream.h"
+
 namespace nsyshid
 {
 	class KamenRiderGateDevice final : public Device
@@ -40,15 +42,29 @@ namespace nsyshid
 	class RideGateUSB
 	{
 	  public:
+		struct RiderFigure final
+		{
+			std::unique_ptr<FileStream> dimFile;
+			std::array<uint8, 0x2D * 0x04> data{};
+			std::array<uint8, 7> uid{};
+			bool present = false;
+			void Save();
+		};
+
 		void SendCommand(std::span<const uint8> buf);
 		std::array<uint8, 64> GetStatus();
 
 	  protected:
 		std::mutex m_kamenRiderMutex;
+		std::array<RiderFigure, 7> m_figures{};
 
 	  private:
+		void GetListTags(std::array<uint8, 64>& replyBuf, uint8 command, uint8 sequence);
+		void QueryBlock(std::array<uint8, 64>& replyBuf, uint8 command, uint8 sequence,
+						const std::array<uint8, 7> uid, uint8 block, uint8 line);
 		uint8 GenerateChecksum(const std::array<uint8, 64>& data,
 							   int numOfBytes) const;
+		std::optional<RiderFigure&> GetFigureByUID(const std::array<uint8, 7> uid);
 
 		std::queue<std::array<uint8, 64>> m_figureAddedRemovedResponses;
 		std::queue<std::array<uint8, 64>> m_queries;
